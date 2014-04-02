d8f34e8aa4c17e218498254f04c7a7d9a2f268d2
Tom <tom@procterweb.co.uk>
2014-04-02 20:45:04 +0100

    Drink diary now posts to server.

--- src/procter/thomas/amulet/DrinkDiaryActivity.java
+++ src/procter/thomas/amulet/DrinkDiaryActivity.java
@@ -2,30 +2,41 @@ package procter.thomas.amulet;
 
 import java.util.ArrayList;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import procter.thomas.amulet.OnRetrieveHTTPData.OnRetrieveHttpData;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.Time;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class DrinkDiaryActivity extends Activity{
+public class DrinkDiaryActivity extends Activity implements OnRetrieveHttpData{
 
 	ArrayList<String> addedDrinks;
 	ArrayList<String> existingDrinks;
+	ArrayList<String> addedQuantities;
+	ArrayAdapter<String> dataAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_drink_diary);
 		addedDrinks = new ArrayList<String>();
 		existingDrinks = new ArrayList<String>();
+		addedQuantities = new ArrayList<String>();
 		existingDrinks.add("Stella");
 		existingDrinks.add("Guiness");
 		setupList();
@@ -36,7 +47,11 @@ public class DrinkDiaryActivity extends Activity{
 		String[] drinks = new String[addedDrinks.size()];
 		addedDrinks.toArray(drinks);
 		
-		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+		for(int i = 0; i < addedDrinks.size(); i++){
+			drinks[i] = addedQuantities.get(i) + " x "+ drinks[i];
+		}
+		
+		dataAdapter = new ArrayAdapter<String>(this,
 				R.layout.custom_list_view, drinks);
 			 
 			  ListView listView = (ListView) findViewById(R.id.lstAddedDrinks);
@@ -48,8 +63,6 @@ public class DrinkDiaryActivity extends Activity{
 				   @Override
 				   public void onItemClick(AdapterView<?> listView, View view, 
 				     int position, long id) {
-				   
-					   
 					   confirmation(position);
 					   
 				   }
@@ -66,6 +79,7 @@ public class DrinkDiaryActivity extends Activity{
 	        public void onClick(DialogInterface dialog, int which) { 
 	            // continue with delete
 	        	addedDrinks.remove(rowId);
+	        	addedQuantities.remove(rowId);
 	        	setupList();
 	        }
 	     });
@@ -111,6 +125,7 @@ private void addDrink(){
 	        public void onClick(DialogInterface dialog, int which) { 
 	            // continue with delete
 	        	addedDrinks.add(mSpinner.getSelectedItem().toString());
+	        	addedQuantities.add(mTextView.getText().toString());
 	        	setupList();
 	        }
 	     });
@@ -119,18 +134,74 @@ private void addDrink(){
 	            // do nothing
 	        }
 	     });
-	    
-
-     // reference UI elements from my_dialog_layout in similar fashion
-
-     // show it
+	 
      builder.show();
      
-		
 	}
 	
 	public void addButton(View view){
 		addDrink();
 		setupList();
+		
 	}
+	
+	public void submitButton(View view){
+		postToServer();
+		addedDrinks.clear();
+		addedQuantities.clear();
+		setupList();
+		
+	}
+	
+	private void postToServer(){
+		JSONObject obj = entryObject();
+		String HTTPString = obj.toString();
+		RetrieveHTTPDataAsync retrieveData = new RetrieveHTTPDataAsync(this);
+		retrieveData.execute("POST", "http://08309.net.dcs.hull.ac.uk/api/admin/drink", HTTPString);
+		
+	}
+	
+	private JSONObject entryObject(){
+		JSONObject obj = new JSONObject();
+		JSONObject entries = new JSONObject();
+		JSONArray entryArray = new JSONArray();
+		
+		String username = SharedPreferencesWrapper.getFromPrefs(this, "username", "default");
+		String password = SharedPreferencesWrapper.getFromPrefs(this, "password", "default");
+		
+		
+		Time time = new Time();
+		time.setToNow();
+		String timeStamp = time.format("%Y-%m-%d %H:%M:%S");
+		
+		try {
+			for(int i = 0; i < addedDrinks.size(); i++){
+				entries.put("timestamp",  timeStamp);
+				entries.put("drinktype",  addedDrinks.get(i));
+				entries.put("unitsconsumed",  addedQuantities.get(i));
+				entryArray.put(entries);
+			}
+			
+			obj.put("username", username);
+			obj.put("password", password);
+			obj.put("entries", entryArray);
+			
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Log.i("tag", obj.toString());
+		
+		return obj;
+	}
+
+	@Override
+	public void onTaskCompleted(String httpData) {
+		// TODO Auto-generated method stub
+		Log.i("onTaskCompleted", httpData);
+		
+	}
+	
+	
 }
