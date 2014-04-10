b8e9c9aee6b1acabecded40f889e9fba5d14c468
Tom <tom@procterweb.co.uk>
2014-04-10 08:38:03 +0100

    Sorted sizing and speed of objects for a playable game experience.

--- src/procter/thomas/amulet/DrawingPanel.java
+++ src/procter/thomas/amulet/DrawingPanel.java
@@ -1,7 +1,6 @@
 package procter.thomas.amulet;
 
 import java.security.SecureRandom;
-import java.util.Random;
 
 import android.content.Context;
 import android.content.Intent;
@@ -21,39 +20,23 @@ public class DrawingPanel extends SurfaceView implements SurfaceHolder.Callback{
 	private MainThread thread;
 	private PilotShape pilotPlayer;
 	private PilotShape[] badGuys;
-	private final int noOfBadGuys = 8;
+	private final int noOfBadGuys = 6;
 	private boolean touchedSquare = false;
 	private Time startTime, endTime;
-	
+	private boolean setObjects = true;
+	private int badGuySize = 500;
+	private int playerSize = 200;
+	private boolean stopOnTouch = false;
 	public DrawingPanel(Context context) {
 		super(context);
 		// adding the callback (this) to the surface holder to intercept events
 		getHolder().addCallback(this);
-		
 		// create the game loop thread
 		thread = new MainThread(getHolder(), this);
-
+		
 		// make the GamePanel focusable so it can handle events
 		setFocusable(true);
-		Paint playerColour = new Paint();
-		playerColour.setColor(0xffffffff);
-		pilotPlayer = new PilotShape((this.getWidth()/2), (this.getHeight()/2), 200, 200, playerColour, null);
-		badGuys = new PilotShape[noOfBadGuys];
-		Paint enemyColour = new Paint();
-		enemyColour.setColor(0xffffff00);
-		for(int i = 0; i < badGuys.length; i++){
-			int dirX;
-			int dirY;
-			do{
-			SecureRandom random = new SecureRandom();
-			dirX = (random.nextInt(3)-1);
-			dirY = (random.nextInt(3)-1);
-			}while(dirX == 0 && dirY == 0);
-			Log.i("dir", dirX + ",  " + dirY);
-			badGuys[i] = new PilotShape((i+2)*100, (i+2)*100, 20, 30, enemyColour, new Point(dirX, dirY));
-			
-			
-		}
+		
 		startTime = new Time();
 		endTime = new Time();
 		startTime.setToNow();
@@ -82,10 +65,43 @@ public class DrawingPanel extends SurfaceView implements SurfaceHolder.Callback{
 			}
 		}
 	}
+	
+	private void setObjects(Canvas canvas) {
+		Paint playerColour = new Paint();
+		playerColour.setColor(0xffffffff);
+		pilotPlayer = new PilotShape((canvas.getWidth() - playerSize),
+				(canvas.getHeight() - playerSize), playerSize, playerSize, playerColour, null);
+		badGuys = new PilotShape[noOfBadGuys];
+		Paint enemyColour = new Paint();
+		enemyColour.setColor(0xffffff00);
+		for (int i = 0; i < badGuys.length; i++) {
+			int dirX;
+			int dirY;
+			int speed = i+1;
+			int height;
+			int width;
+			do {
+				SecureRandom random = new SecureRandom();
+				dirX = (random.nextInt(3) - 1);
+				dirY = (random.nextInt(3) - 1);
+			} while (dirX == 0 || dirY == 0);
+			Log.i("dir", dirX + ",  " + dirY);
+			do {
+				SecureRandom random = new SecureRandom();
+				height = 50* (random.nextInt(10) + 1);
+				width = 50 * (random.nextInt(10) + 1);
+			} while (height+width != badGuySize);
+			Log.i("size", height + ",  " + width);
+			badGuys[i] = new PilotShape((badGuySize/2)+1, (badGuySize/2)+1, width, height,
+					enemyColour, new Point(dirX*speed, dirY*speed));
+
+			this.setObjects = false;
+		}
+	}
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
-		
+		if(!stopOnTouch){
 		if (event.getAction() == MotionEvent.ACTION_DOWN) {
 			
 			if(pilotPlayer.getRect().contains((int)event.getX(), (int)event.getY())){
@@ -102,45 +118,56 @@ public class DrawingPanel extends SurfaceView implements SurfaceHolder.Callback{
 				pilotPlayer.setPosition((int)event.getX(), (int)event.getY());
 			}
 		}
+		}
 		return true;
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
+		
+		
 		canvas.drawRGB(43, 223, 255);
 		
 		for(int i = 0; i < badGuys.length; i++){
 			canvas.drawRect(badGuys[i].getRect(), badGuys[i].colour);
 		}
 		canvas.drawRect(pilotPlayer.getRect(), pilotPlayer.colour);
-		
-	}
-	
-	public void onUpdate(Canvas canvas){
-		for(int i = 0; i < badGuys.length; i++){
-			Point dir = badGuys[i].getDirection();
-			badGuys[i].setPosition(badGuys[i].centre.x + dir.x, badGuys[i].centre.y + dir.y);
-			
-			if(badGuys[i].getRect().left < 0 || badGuys[i].getRect().right > canvas.getWidth()){
-				badGuys[i].setDirection(new Point(-dir.x, dir.y));
-			}
-			if(badGuys[i].getRect().top < 0 || badGuys[i].getRect().bottom > canvas.getHeight()){
-				badGuys[i].setDirection(new Point(dir.x, -dir.y));
-			}
-			Rect goodGuyRect = pilotPlayer.getRect();
-			Rect badGuyRect = badGuys[i].getRect();
-			if(Rect.intersects(goodGuyRect, badGuyRect)){
-				pilotPlayer.colour.setColor(0xffff0000);
-				finish();
-			}
 		}
+	
+	
+	public void onUpdate(Canvas canvas) {
+		
+			if (setObjects) {
+				setObjects(canvas);
+			}
+
+			for (int i = 0; i < badGuys.length; i++) {
+				Point dir = badGuys[i].getDirection();
+				badGuys[i].setPosition(badGuys[i].centre.x + dir.x,
+						badGuys[i].centre.y + dir.y);
+
+				if (badGuys[i].getRect().left < 0
+						|| badGuys[i].getRect().right > canvas.getWidth()) {
+					badGuys[i].setDirection(new Point(-dir.x, dir.y));
+				}
+				if (badGuys[i].getRect().top < 0
+						|| badGuys[i].getRect().bottom > canvas.getHeight()) {
+					badGuys[i].setDirection(new Point(dir.x, -dir.y));
+				}
+				Rect goodGuyRect = pilotPlayer.getRect();
+				Rect badGuyRect = badGuys[i].getRect();
+				if (Rect.intersects(goodGuyRect, badGuyRect)) {
+					pilotPlayer.colour.setColor(0xffff0000);
+					finish();
+				}
+			}
+		
 	}
 	
 	private void finish(){
-		
+		stopOnTouch = true;
 		endTime.setToNow();
 		int newTime = (int)(endTime.toMillis(false) - startTime.toMillis(false))/1000;
-		Log.i("Time", String.valueOf(newTime));
 		Intent intent = new Intent(super.getContext(), ResultsActivity.class);
 		intent.putExtra("score", newTime);
 		intent.putExtra("task", "Pilot");
